7b4d9af52a7bfce01f59166fcf77d668b7149bea
Brittany Gahan <brittanygahan89@gmail.com>
2022-06-16 18:59:56 -0400

    initial set up

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
 const questions = [];
@@ -7,7 +10,13 @@ const questions = [];
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    return inquirer.prompt([
+        {
+            
+        }
+    ])
+}
 
 // Function call to initialize app
 init();
